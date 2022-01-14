e124d1e52e14357dc68eef916de8de054ef30f48
yunikpandey <86758131+yunikpandey@users.noreply.github.com>
2022-01-14 20:56:29 +0545

    Update task2.py

--- Hw/task2.py
+++ Hw/task2.py
@@ -1,46 +1,28 @@
-read = []
-def intro():
-    count = 0
-    print("Swallow Speed Analysis: Version 1.0")
-    while True:
-        count += 1
-        data = input("Enter next reading: ")
-        if data == "":
-            break
-        else:
-            check(read,count)
-        
-def check(data,count):
-    if data[0] == "U" :
-        a = float(data[1:])*1.61
-        read.append(a)
-        print("Reading Saved")
-    elif data[0] == "E":
-        a = float(data[1:])
-        read.append(a)
+from statistics import mean
+print("Swallow Speed Analysis: Version 1.0 ")
+km = []
+while True:
+    read = input("Enter next reading: ")
+    if read == "":
+        break
+    elif read[0] == "U":
+        km.append(float(read[1:])*1.61)
         print("Reading saved")
-    my_fun(read,count)
-    
-def my_fun(read,count):
-    
-    max_km = max(read)
-    con_km_to_miles = max_km/1.61
-        
-    min_km = min(read)
-    min_km_to_miles = min_km/1.61
-    sum = 0
-    for i in range(len(read)):
-        sum += read[i]
-    avg = sum/len(read)
-    convert = avg /1.61
-    print("Result Summary")
-    if count == 2:
-        print(count-1," Reading saved")
-    else:
-        print(count-1," Reading saved")   
-        
-    print("Max speed is %.1f KPH and %.1f MPH"%(max_km,con_km_to_miles))
-    print("Min speed is %.1f KPH and %.1f MPH"%(min_km,min_km_to_miles))
-    print("Average speed is %.1f KPH and %.1f MPH"%(avg,convert))
-                
-intro()
+    elif read[0] == "E":
+        km.append(float(read[1:]))
+        print("Reading saved")
+
+if len(km) == 0:
+    print("No reading entered. No thing to do.")
+else:
+    maximum_km = max(km)
+    maximum_miles = maximum_km/1.61
+    minimum_km = min(km)
+    minimum_miles = minimum_km/1.61
+    average_km = mean(km)
+    average_miles = average_km/1.61
+    print("\nReading Summary\n")
+    print(len(km)," Readings Analysed\n")
+    print(f"Maximum speed {maximum_miles:.2f} MPH, {maximum_km:.2f} KPH")
+    print(f"Minimum speed {minimum_miles:.2f} MPH, {minimum_km:.2f} KPH")
+    print(f"Average speed {average_miles:.2f} MPH, {average_km:.2f} KPH")
